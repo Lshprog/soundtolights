fd3e5bd2e4563c59a9e38b26acbc701578b31f66
onisalex1@gmail.com <onisalex1@gmail.com>
2023-03-25 03:30:20 +0800

    creating SongToRbg object

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import numpy as np
 
 import class_for_audio
-#from convert_to_rgb import *
+from matplotlib import pyplot as plt
+import numpy
+
+import convert_to_rgb
+
 
 song1 = class_for_audio.SongToRgb("song1.mp3")
 song1.convert_to_numerical()
 wv = song1.get_array_splitwaves(15)
 
+y = numpy.arange(0,len(wv))
+plt.plot(y,wv)
+plt.show()
+
+
+
 for i in range(len(wv)):
-    print(str(wv[i]))
+    print(str(convert_to_rgb.wave2rgb(wv[i])))
+
+
 
 
