10d63313666154fa378024ed13a37c877e79096b
onisalex1@gmail.com <onisalex1@gmail.com>
2023-03-25 03:30:55 +0800

    changed the algorithm (still can bee improved)

--- convert_to_rgb.py
+++ convert_to_rgb.py
@@ -1,43 +1,42 @@
-#NEED TO BE CHANGED.
-
 def wave2rgb(wave):
     # This is a port of javascript code from  http://stackoverflow.com/a/14917481
     gamma = 0.8
     intensity_max = 1
 
-    if wave < 380:
+    if wave < 30:
         red, green, blue = 0, 0, 0
-    elif wave < 440:
-        red = -(wave - 440) / (440 - 380)
-        green, blue = 0, 1
-    elif wave < 490:
-        red = 0
-        green = (wave - 440) / (490 - 440)
+    elif wave < 250:
+        red = -(wave - 250) / (500 - 30)
+        green = 1
+        blue = (wave - 30)/(2500-250)
+    elif wave < 500:
+        red = (wave - 30) / (2500 - 500)
+        green = (wave - 500) / (250 - 500)
         blue = 1
-    elif wave < 510:
+    elif wave < 1000:
         red, green = 0, 1
-        blue = -(wave - 510) / (510 - 490)
-    elif wave < 580:
-        red = (wave - 510) / (580 - 510)
+        blue = -(wave - 1000) / (1000 - 500)
+    elif wave < 2500:
+        red = (wave - 500) / (2500 - 1000)
         green, blue = 1, 0
-    elif wave < 645:
+    elif wave < 5000:
         red = 1
-        green = -(wave - 645) / (645 - 580)
-        blue = 0
-    elif wave <= 780:
+        green = -(wave - 5000) / (5000 - 2500)
+        blue = -(wave - 5000) / (4800 - 1000)
+    elif wave <= 10000:
         red, green, blue = 1, 0, 0
     else:
-        red, green, blue = 0, 0, 0
+        red, green, blue = 1, 1, 1
 
     # let the intensity fall of near the vision limits
-    if wave < 380:
+    if wave < 30:
         factor = 0
-    elif wave < 420:
-        factor = 0.3 + 0.7 * (wave - 380) / (420 - 380)
-    elif wave < 700:
+    elif wave < 2500:
+        factor = 0.1 + 0.4 * (wave - 30) / (2000 - 30)
+    elif wave < 5000:
         factor = 1
-    elif wave <= 780:
-        factor = 0.3 + 0.7 * (780 - wave) / (780 - 700)
+    elif wave <= 10000:
+        factor = 0.1 + 0.4 * (10000 - wave) / (10000 - 5000)
     else:
         factor = 0
 
